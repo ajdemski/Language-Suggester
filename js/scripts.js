b1e29089022498ac30dda3eb92ba7fb241b9bca3
Aaron Demski <ajdemski@frontier.com>
2023-11-05 17:43:48 -0800

    add ui logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Interface Logic
 
-function suggestionGenerator (option1, option2, option3, scale, time) {
+function suggestionGenerator(option1, option2, scale, time) {
   if (option1 === "saturn" && scale >= 5 && time >= 3) {
     return "You should learn C#!";
   } else if (option2 === "Mount Everest" && scale >= 7 && time >= 5) {
@@ -10,6 +10,21 @@ function suggestionGenerator (option1, option2, option3, scale, time) {
   }
 }
 
-
 // User Interface Logic
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("survey").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const option1 = document.getElementById("options-1").value;
+    const option2 = document.getElementById("options-2").value;
+    const option3 = document.getElementById("options-3").value;
+    const scale = parseInt(document.getElementById("scale").value);
+    const time = parseInt(document.getElementById("time").value);
+
+    const language = document.getElementById("language");
+    const result = document.getElementById("result");
+
+    result.textContent = suggestionGenerator(option1, option2, option3, scale, time);
+  });
+});
