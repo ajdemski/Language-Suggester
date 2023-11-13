14a31ee0eaffef47bddbe29c2a6f30c78ecf0898
Aaron Demski <ajdemski@frontier.com>
2023-11-12 19:59:52 -0800

    fix un-used code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // Business Interface Logic
 
-function suggestionGenerator(option1, option2, scale, time) {
+function suggestionGenerator(option1, option2, option3, scale, time) {
   if (option1 === "saturn" && scale >= 5 && time >= 3) {
     return "You should learn C#!";
-  } else if (option2 === "Mount Everest" && scale >= 7 && time >= 5) {
+  } else if (option2 === "mountEverest" && scale >= 7 && time >= 5) {
     return "You should learn JavaScript!";
   } else {
     return "You should learn Python!";
@@ -21,8 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const option3 = document.getElementById("options-3").value;
     const scale = parseInt(document.getElementById("scale").value);
     const time = parseInt(document.getElementById("time").value);
-
-    const language = document.getElementById("language");
     const result = document.getElementById("result");
 
     result.textContent = suggestionGenerator(option1, option2, option3, scale, time);
